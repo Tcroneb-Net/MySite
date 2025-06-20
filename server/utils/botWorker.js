bccd49e3bb3d57886f98aa79c025ee51b2040807
LIL CRONE <tcronebtech2015@gmail.com>
2025-06-20 02:19:37 +0200

    Create botWorker.js

---
+++ server/utils/botWorker.js
@@ -0,0 +1,4 @@
+// Placeholder for bot runner
+// This script can be extended to dynamically launch bots using child_process or node-telegram-bot-api
+
+console.log('Bot worker script loaded. Add deployment logic here.');
